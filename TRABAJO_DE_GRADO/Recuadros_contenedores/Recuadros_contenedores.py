e72d06d21016b5eef44be2f10811ab6ed1eb8ab3
Diego <ramirezvargasdiegoalejandro2@gmail.com>
2020-11-23 14:57:58 -0500

    recuadros contenedores de las detecciones

--- TRABAJO_DE_GRADO/Recuadros_contenedores/Recuadros_contenedores.py
+++ TRABAJO_DE_GRADO/Recuadros_contenedores/Recuadros_contenedores.py
@@ -7,15 +7,25 @@ import errno
 def load_obj(name ):
     with open( name, 'rb') as f:
         return pickle.load(f)
-detection_images= load_obj('/home/diego/TRABAJO-DE-GRADO-PREGRADO-UMV/TRABAJO_DE_GRADO/darknet/detect_images_info/informacion_imagenes.pkl')
-path_imagenes_boundingbox='/home/diego/TRABAJO-DE-GRADO-PREGRADO-UMV/TRABAJO_DE_GRADO/Recuadros_contenedores/resultados_con_recuadros'
-path_imagenes='/home/diego/TRABAJO-DE-GRADO-PREGRADO-UMV/TRABAJO_DE_GRADO/Annotation_images/IMAGENES_ETIQUETADAS/vott-csv-export/'
+
+detection_images= load_obj('/home/diego/TRABAJO-DE-GRADO-PREGRADO-UMV/TRABAJO_DE_GRADO/darknet/detect_images_info/informacion_imagenes_test_multi_clase.pkl')
+path_imagenes_boundingbox='/home/diego/TRABAJO-DE-GRADO-PREGRADO-UMV/TRABAJO_DE_GRADO/Recuadros_contenedores/resultados_multi_clase'
+path_imagenes='/home/diego/TRABAJO-DE-GRADO-PREGRADO-UMV/TRABAJO_DE_GRADO/Performance_parameters/imagenes_test_multi_clase/'
+
+
+
+
+
+
 try:
     os.mkdir(path_imagenes_boundingbox) 
 except OSError as e:
     if e.errno != errno.EEXIST:
         raise
 color_0 = (255,0,0)
+color_1 = (0,0,255)
+color_2= (0,255,0)
+color_3=(255,0,255)
 for imagen_info in detection_images:
     if imagen_info['detections']:
         img=cv2.imread(imagen_info['image_name'])
@@ -27,16 +37,28 @@ for imagen_info in detection_images:
             sup_izq=(xCoord, yCoord)
             inf_der=(xCoord + xEntent, yCoord + yExtent)
             sup_izq_titulo=(xCoord, yCoord-30)
-            cv2.rectangle(img,sup_izq,inf_der, color_0, 3)
+            
+            clase=imagen_info['detections'][detection_number][0]
+            if clase=='hueco':
+                color=color_0
+            if clase == 'grieta':
+                color=color_1
+            if clase == 'piel_de_cocodrilo':
+                color=color_2
+            if clase== 'parche':
+                color=color_3
+                
+            cv2.rectangle(img,sup_izq,inf_der, color, 2)
+            
             clase=imagen_info['detections'][detection_number][0]
             proba=imagen_info['detections'][detection_number][1]
-            cv2.putText(img,clase+' '+str("{:.2f}".format(proba*100))+'%', sup_izq_titulo, cv2.FONT_HERSHEY_SIMPLEX, 1, color_0, 2)
+            #cv2.putText(img,clase+' '+str("{:.2f}".format(proba*100))+'%', sup_izq_titulo, cv2.FONT_HERSHEY_SIMPLEX, 1, color_0, 2)
         
         img_g=cv2.resize(img,(1000,700))
-        cv2.imwrite(os.path.join(path_imagenes_boundingbox,imagen_info['image_name'].split("/")[-1]),img_g)
+        cv2.imwrite(os.path.join(path_imagenes_boundingbox,imagen_info['image_name'].split("/")[-1]),img)
         
     if not imagen_info['detections']:    
         img=cv2.imread(imagen_info['image_name'])
         img_g=cv2.resize(img,(1000,700))
-        cv2.imwrite(os.path.join(path_imagenes_boundingbox,imagen_info['image_name'].split("/")[-1]),img_g)
+        cv2.imwrite(os.path.join(path_imagenes_boundingbox,imagen_info['image_name'].split("/")[-1]),img)
         
